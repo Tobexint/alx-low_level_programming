79384e6f6a5aead8667f8c860b09812ea2b7733b
Tobexint <tobexint@gmail.com>
2022-04-14 15:58:58 -0700

    Super

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,8 +11,8 @@ int main(void)
 char c;
 
 c = 'A';
-_putchar("%c: %d\n", c, _isupper(c));
+putchar("%c: %d\n", c, _isupper(c));
 c = 'a';
-_putchar("%c: %d\n", c, _isupper(c));
+putchar("%c: %d\n", c, _isupper(c));
 return (0);
 }
