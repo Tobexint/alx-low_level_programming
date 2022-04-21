d10ca666afbd24813f8be3e75c4ced829e1b4f75
Tobexint <tobexint@gmail.com>
2022-04-21 02:55:22 -0700

    Prototypes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef main_H
+#define main_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +11,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void _putchar(char c);
+int _atoi(char *s);
+#endif /*main.h*/
