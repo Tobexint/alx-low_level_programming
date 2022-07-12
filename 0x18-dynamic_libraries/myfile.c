cebdef00416f9cc212ef2def3ffa507d6e18b116
Tobexint <tobexint@gmail.com>
2022-07-11 19:08:33 -0700

    c

---
+++ 0x18-dynamic_libraries/myfile.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add- calculates sum of two numbers
+ * @a: number
+ * @b: number
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * subtract- calculates the difference between two numbers
+ * @a: number
+ * @b: number
+ *
+ * Return: difference
+ */
+int subtract(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * multiply- calculates the multiplication of two numbers
+ * @: number
+ * @b: number
+ *
+ * Return: multiplication
+ */
+int multiply(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ *divide- calculates the division of two numbers
+ * @: number
+ * @b: number
+ *
+ * Return: division
+ */
+int divide(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod- calculates the remainder of two numbers
+ * @: number
+ * @b: number
+ *
+ * Return: modulus
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
