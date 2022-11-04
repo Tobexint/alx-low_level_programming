797638f0a6e15025149edb464c02ef41a76034e0
tobexint <tobexint@gmail.com>
2022-11-04 02:02:07 -0700

    jup

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,55 +1,44 @@
 #include "search_algos.h"
 #include <math.h>
-/**
-*jump_search - searches for a value in a sorted array of integers using the
-*jump search algorithm
-*@array: pointer to the array to search
-*@size: the size of the array
-*@value: the value to search for
-*
-*Return: the index where the value is located or -1 if not found
-*/
 
+/**
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step = sqrt(size);
-	size_t prev = 0;
-	size_t i = 0;
+	int index, m, k, prev;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (i * step <= size)
-	{
-		if (array[i * step] < value)
-		{
-			printf("Value checked array[%lu] ", i * step);
-			printf("= [%i]\n", array[i * step]);
-			i++;
-			if (i != 0)
-				prev = i - 1;
-		}
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
 
-		else
-		{
-			printf("Value found between ");
-			printf("indexes [%lu] ", prev * step);
-			printf("and [%lu]\n", i * step);
-			prev = prev * step;
-			while (prev <= i * step)
-			{
-				printf("Value checked array[%lu] ", prev);
-				printf("= [%i]\n", array[prev]);
-				if (array[prev] == value)
-					return (prev);
-				prev++;
-			}
-			return (-1);
-		}
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
-	printf("Value found between indexes [%lu] ", prev * step);
-	printf("and [%lu]\n", i * step);
-	printf("Value checked array[%lu] ", prev * step);
-	printf("= [%i]\n", array[prev * step]);
+
 	return (-1);
 }
